23719c6e32fdf3cccd3967099d4cc41037629cf3
Cenyo <cenyo.medewou@gmail.com>
2020-09-10 23:54:47 +0200

    loading properties from file and resource

--- src/main/java/Requests/DsmAuth.java
+++ src/main/java/Requests/DsmAuth.java
@@ -1,6 +1,27 @@
 package Requests;
 
+import Exeptions.DsmLoginException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class DsmAuth {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DsmAuth.class.getName());
+
+    private static final String HOST_KEY = "host";
+    private static final String PORT_KEY = "port";
+    private static final String USERNAME_KEY = "username";
+    private static final String PASSWORD_KEY = "password";
 
     private String host;
     private Integer port;
@@ -44,6 +65,78 @@ public class DsmAuth {
     }
 
     public static DsmAuth of(String host, Integer port, String userName, String password) {
+
+        Optional.ofNullable(host).orElseThrow(() -> new DsmLoginException("Unable to find property : host"));
+        Optional.ofNullable(port).orElseThrow(() -> new DsmLoginException("Unable to find property : port"));
+        Optional.ofNullable(userName).orElseThrow(() -> new DsmLoginException("Unable to find property : userName"));
+        Optional.ofNullable(password).orElseThrow(() -> new DsmLoginException("Unable to find property : password"));
+
         return new DsmAuth().setHost(host).setPort(port).setUserName(userName).setPassword(password);
+    }
+
+    public static DsmAuth fromResource(String fileName) {
+        try {
+            Map<String, String> properties = getPropertiesFromResource(fileName);
+
+            validate(properties);
+
+            return DsmAuth.of(properties.get(HOST_KEY), Integer.valueOf(properties.get(PORT_KEY)), properties.get(USERNAME_KEY), properties.get(PASSWORD_KEY));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        throw new DsmLoginException("Unable to login with the provided informations ");
+    }
+
+    public static DsmAuth fromFile(File file) throws IOException {
+
+        try {
+            Map<String, String> properties = getPropertiesFromFile(file);
+
+            validate(properties);
+
+            return DsmAuth.of(properties.get(HOST_KEY), Integer.valueOf(properties.get(PORT_KEY)), properties.get(USERNAME_KEY), properties.get(PASSWORD_KEY));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        throw new DsmLoginException("Unable to login with the provided informations ");
+    }
+
+    public static  Map<String, String> getPropertiesFromFile(File file) throws IOException {
+        return Files.readAllLines(file.toPath(), StandardCharsets.UTF_8).stream()
+                .filter(l -> !l.isEmpty())
+                .peek(LOGGER::info)
+                .collect(Collectors.
+                        toMap(
+                                l -> l.substring(0,l.indexOf("=")),
+                                l -> l.substring(l.indexOf("=")+1)
+                        )
+                );
+    }
+
+
+    private static Map<String, String> getPropertiesFromResource(String fileName) throws IOException, URISyntaxException {
+        ClassLoader classLoader = DsmAuth.class.getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+
+        Optional.ofNullable(resource).orElseThrow(() -> new IllegalArgumentException("file not found! " + fileName));
+
+        File file = new File(resource.toURI());
+        return getPropertiesFromFile(file);
+    }
+
+    private static void validate(Map<String, String> properties) {
+
+        String host = properties.get(HOST_KEY) != null && !properties.get(HOST_KEY).isEmpty() ? properties.get(HOST_KEY) : null;
+        Integer port = properties.get(PORT_KEY) != null && !properties.get(PORT_KEY).isEmpty() ? Integer.valueOf(Optional.ofNullable(properties.get(PORT_KEY)).orElse(null)) : null;
+        String userName = properties.get(USERNAME_KEY) != null && !properties.get(USERNAME_KEY).isEmpty() ? properties.get(USERNAME_KEY) : null;
+        String password = properties.get(PASSWORD_KEY) != null && !properties.get(PASSWORD_KEY).isEmpty() ? properties.get(PASSWORD_KEY) : null;
+
+        if(host != null && port != null && userName != null && password != null) {
+            return;
+        }
+
+        throw new DsmLoginException("None of the properties can't be empty : host="+ host +", port="+ port + ", username=" +userName+ ", password="+password);
     }
 }
