67e323dfe73e9f25cce619615232d08890600e06
Cenyo <cenyo.medewou@gmail.com>
2020-09-11 09:19:56 +0200

    Add more tests to DsmLoginRequest

--- src/test/java/DsmLoginTest.java
+++ src/test/java/DsmLoginTest.java
@@ -1,13 +1,22 @@
+import Exeptions.DsmException;
 import Exeptions.DsmLoginException;
 import Requests.DsmAuth;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class DsmLoginTest {
 
+    DsmAuth auth;
+
+    @Before
+    public void initTest() {
+        auth = DsmAuth.fromResource("env.properties");
+    }
+
     @Test
     public void testLoginFromResourceSuccess() {
-        DsmClient client = DsmClient.login(DsmAuth.fromResource("env.properties"));
+        DsmClient client = DsmClient.login(auth);
 
         Assert.assertNotNull(client);
         Assert.assertNotNull(client.getSID());
@@ -19,9 +28,31 @@ public class DsmLoginTest {
     }
 
     @Test(expected = DsmLoginException.class)
-    public void testLoginAuthenticationFailed() {
-        DsmClient.login(DsmAuth.fromResource("env-login-failed.properties"));
-
+    public void testLoginAuthenticationWrongUserName() {
+        DsmAuth dsmAuth = auth
+                .setUserName("username")
+                .setPassword("password");
+        DsmClient.login(dsmAuth);
     }
 
+    @Test(expected = DsmLoginException.class)
+    public void testLoginAuthenticationWrongPassword() {
+        DsmAuth dsmAuth = auth
+                .setPassword("password");
+        DsmClient.login(dsmAuth);
+    }
+
+    @Test(expected = DsmException.class)
+    public void testLoginAuthenticationWrongPort() {
+        DsmAuth dsmAuth = auth
+                .setPort(5444);
+        DsmClient.login(dsmAuth);
+    }
+
+    @Test(expected = DsmException.class)
+    public void testLoginAuthenticationWrongHost() {
+        DsmAuth dsmAuth = auth
+                .setHost("host");
+        DsmClient.login(dsmAuth);
+    }
 }
