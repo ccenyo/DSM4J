c178c0453537a6cb40d3342272c98a7b2f52edf8
Cenyo <cenyo.medewou@gmail.com>
2020-09-17 09:48:03 +0200

    resolve sonarqube issues

--- src/main/java/utils/DsmUtils.java
+++ src/main/java/utils/DsmUtils.java
@@ -103,20 +103,29 @@ public class DsmUtils {
     }
 
     public static File downloadFile(InputStream inputStream, String destinationPath) throws IOException {
-        File file = new File(destinationPath);
-        FileOutputStream fos = new FileOutputStream(file);
-        int inByte;
-        while((inByte = inputStream.read()) != -1)
-            fos.write(inByte);
-        inputStream.close();
-        fos.close();
+        File file;
+        FileOutputStream fos = null;
+        try {
+            file = new File(destinationPath);
+            fos = new FileOutputStream(file);
+            int inByte;
+            while((inByte = inputStream.read()) != -1)
+                fos.write(inByte);
+        } catch (IOException e) {
+            throw new DsmException("");
+        } finally {
+            inputStream.close();
+            if(fos != null) {
+                fos.close();
+            }
+        }
         return file;
     }
 
     public static String extractFileName(String path) {
        String completePath = Optional.ofNullable(path).orElseThrow(() -> new DsmException("Unable to get file name"));
 
-       return completePath.substring(completePath.lastIndexOf("/")+1);
+       return completePath.substring(completePath.lastIndexOf('/')+1);
     }
 
 }
