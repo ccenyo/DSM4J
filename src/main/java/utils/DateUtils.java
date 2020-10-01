5bbe85f6e368084ecfb1209e28d614e3a645b6a3
Cenyo <cenyo.medewou@gmail.com>
2020-10-01 21:21:25 +0200

    fix sonar

--- src/main/java/utils/DateUtils.java
+++ src/main/java/utils/DateUtils.java
@@ -7,7 +7,7 @@ import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
 
-    private static final String DsmDateFormat = "YYYY-MM-dd hh:mm:ss";
+    private static final String dsmDateFormat = "YYYY-MM-dd hh:mm:ss";
 
     private DateUtils(){}
 
@@ -17,6 +17,6 @@ public class DateUtils {
     }
 
     public static String convertDateToString(LocalDateTime localDate) {
-        return localDate.format(DateTimeFormatter.ofPattern(DsmDateFormat));
+        return localDate.format(DateTimeFormatter.ofPattern(dsmDateFormat));
     }
 }
